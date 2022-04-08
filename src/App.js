cb6cb2b8604a7759571a80212dfd05b5236ed476
Lev <lev.bubnov@careerist.com>
2022-04-08 23:40:48 +0300

    delete some shit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/MainPage'
-import {TASK_TEST_LIST} from "./constants";
-import {useState, useEffect} from "react";
 
 function App() {
-    //auth
-    // register
-    // mainpage
-    // task list
-    // deadline
-    // assign user
-    // task info (name and description)
-    // is task completed
-    // edit tasks
-    // create and delete tasks
-
-    const [tasks, setTasks] = useState([])
-
-    useEffect(() => {
-        setTimeout(() =>
-            setTasks(TASK_TEST_LIST)
-        )
-
-    }, [])
 
     return (
         <div className="App">
-            <MainPage tasks={tasks}/>
+            <MainPage />
         </div>
     );
 }
